44a7976594699161db817909082089f695c52a46
K-JeongWoo <moke456@naver.com>
2021-10-13 09:48:54 +0900

    1. 테스트 : 수정사항 적용

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,30 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'app'
+  name: 'app',
+  mounted () {
+    window.addEventListener('unload', this.unLoadEvent)
+  },
+  methods: {
+    unLoadEvent: function handler (event) {
+      // if (window.performance.navigation.type !== 1) {
+      //   axios.post(`/api/v1/api/auth/loginout`).then(res => { console.log(res) }).catch(function (error) { console.log(error) })
+      // } else {
+      //   console.log(456)
+      // }
+      if (event.keyCode === 116 || (event.ctrlKey === true && event.keyCode === 82)) {
+        console.log(1)
+      } else {
+        axios.post(`/api/v1/api/auth/loginout`).then(res => { console.log(res) }).catch(function (error) { console.log(error) })
+      }
+      event.preventDefault() // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+      // Chrome requires returnValue to be set
+      event.returnValue = ''
+    }
+  }
 }
 </script>
 
